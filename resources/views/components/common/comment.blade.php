3adcd0475f162c05ab8b91a9964a62d4a155b982
elmehdi el faithi <elmehdielfatihi1111@gmail.com>
2024-01-27 17:33:03 +0100

    adjust comment card styling

--- resources/views/components/common/comment.blade.php
+++ resources/views/components/common/comment.blade.php
@@ -1,4 +1,4 @@
-<section class="shadow-lg rounded-lg p-4 items-start">
+<section class="shadow rounded-lg p-4 items-start bg-white">
   <header class="flex items-start space-x-3 mb-2">
     <img class="rounded-full w-10 h-10" src="https://media.licdn.com/dms/image/C4E03AQFog-dpZdNi4A/profile-displayphoto-shrink_800_800/0/1583457495504?e=2147483647&v=beta&t=13hretZCXqnp8e-D-ftcNRABqF7XXdEHP6ZyX5D1QUo" alt="">
     <div>
