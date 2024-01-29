9b13144b405fdc9de90cca964b64a8e154bba6cf
elmehdi el faithi <elmehdielfatihi1111@gmail.com>
2024-01-29 16:32:48 +0100

    adjust questions tagged view

--- resources/views/pages/questions/tagged/index.blade.php
+++ resources/views/pages/questions/tagged/index.blade.php
@@ -1,7 +1,7 @@
 @php
   /**
    * @var \App\Models\Filter\Filters $filters
-   * @var \Illuminate\Pagination\LengthAwarePaginator $questions
+   * @var \Illuminate\Pagination\LengthAwarePaginator|\App\Models\Question[] $questions
    */
 @endphp
 
@@ -9,38 +9,39 @@
   <x-slot:title>
     {{ sprintf('%s questions - Laraoverflow', $tag->name) }}
   </x-slot:title>
-  <header class="space-y-4 px-4 py-4">
+  <header class="space-y-4 py-10">
     <h1 class="text-2xl font-semibold">
       Questions tagged [{{ $tag->name }}]
     </h1>
     <p class="text-sm">
       {{ $tag->description }}
     </p>
-    <div class="flex justify-between items-center">
-      <p class="text-sm">
-        {{ $questions->total() }} {{ $questions->total() == 1 ? 'question' : 'questions' }}
-      </p>
-      @php
-        $sortFilter = $filters->findByName('sort');
-      @endphp
-
-      <div class="inline-flex rounded" role="group">
-        <a href="{{ $filters->generateFilterLink('sort', 'most_votes') }}"
-           class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s hover:bg-gray-100 hover:bg-gray-700 focus:z-10 focus:ring-2 focus:ring-gray-700 focus:bg-gray-700 {{ $sortFilter?->getValue() === 'most_votes' ? '!bg-gray-200 !bg-gray-600' : '' }}">
-          Most votes
-        </a>
-        <a href="{{ $filters->generateFilterLink('sort', 'newest') }}"
-           class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:bg-gray-700 focus:z-10 focus:ring-2 focus:ring-gray-700 focus:bg-gray-700 {{ $sortFilter?->getValue() === 'newest' ? '!bg-gray-200 !bg-gray-600' : '' }}">
-          Newest
-        </a>
-        <a href="{{ $filters->generateFilterLink('sort', 'unanswered') }}"
-           class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e hover:bg-gray-100 hover:bg-gray-700 focus:z-10 focus:ring-2 focus:ring-gray-700 focus:bg-gray-700 {{ $sortFilter?->getValue() === 'unanswered' ? '!bg-gray-200 !bg-gray-600' : '' }}">
-          Unanswered
-        </a>
+    <div class="flex items-center justify-between">
+      <div class="flex justify-between items-center">
+        @php
+          $sortFilter = $filters->findByName('sort');
+        @endphp
+        <div class="btn-group" role="group">
+          <a href="{{ $filters->generateFilterLink('sort', 'most_votes') }}"
+             class="btn-group-item {{ $sortFilter?->getValue() === 'most_votes' ? 'active' : '' }}">
+            Most votes
+          </a>
+          <a href="{{ $filters->generateFilterLink('sort', 'newest') }}"
+             class="btn-group-item {{ $sortFilter?->getValue() === 'newest' ? 'active' : '' }}">
+            Newest
+          </a>
+          <a href="{{ $filters->generateFilterLink('sort', 'unanswered') }}"
+             class="btn-group-item {{ $sortFilter?->getValue() === 'unanswered' ? 'active' : '' }}">
+            Unanswered
+          </a>
+        </div>
       </div>
+      <a class="btn btn-primary-outlined" href="{{ route('questions.create') }}">
+        Ask Question
+      </a>
     </div>
   </header>
-  <section>
+  <section class="space-y-4">
     @foreach($questions as $question)
       <x-question.card :question="$question"/>
     @endforeach
