9472654db281a0a9dcb6d35b19110b0c8d63f377
elmehdi el faithi <elmehdielfatihi1111@gmail.com>
2024-02-04 22:11:08 +0100

    add nullable to user

--- resources/views/components/blocks/questions/post-viewer.blade.php
+++ resources/views/components/blocks/questions/post-viewer.blade.php
@@ -103,7 +103,7 @@
       @endif
     </form>
     <!-- accepted answer -->
-    @if ($model instanceof \App\Models\Answer && $user->id === $question->user_id && !$question->acceptedAnswer)
+    @if ($model instanceof \App\Models\Answer && !$question->acceptedAnswer && $user?->id === $question->user_id)
       <form
         action='{{ route('questions.answers.accept.store', [$model, $question]) }}'
         method="post"
@@ -131,6 +131,12 @@
           Click to mark this as your accepted answer
         </div>
       </form>
+    @endif
+
+    @if ($model instanceof \App\Models\Answer && $model->accepted)
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-violet-400">
+        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
+      </svg>
     @endif
   </div>
 
